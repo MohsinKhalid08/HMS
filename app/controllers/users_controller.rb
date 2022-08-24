baf62a661ce7fc610be5197c9c153c7ef75f4bd0
Mohsin Khalid <mkmohsinkhalid@gmail.com>
2022-08-24 11:24:13 +0500

    initial commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
     end
   
     def edit
+      @user = User.find(params[:id])
     end
   
     def create
@@ -36,10 +37,10 @@ class UsersController < ApplicationController
       respond_to do |format|
         if @user.update(user_params)
           format.html { redirect_to user_url(@user), notice: "user was successfully updated." }
-          format.json { render :show, status: :ok, location: @user }
+          # format.json { render :show, status: :ok, location: @user }
         else
           format.html { render :edit, status: :unprocessable_entity }
-          format.json { render json: @user.errors, status: :unprocessable_entity }
+          # format.json { render json: @user.errors, status: :unprocessable_entity }
         end
       end
     end
@@ -62,7 +63,8 @@ class UsersController < ApplicationController
   
       # Only allow a list of trusted parameters through.
       def user_params
-        params.require(:user).permit(:name, :phone, :emergencycontact, :email,:usertype,:password,:confirmed_password)
+        params.require(:user).permit(:name, :phone, :emergencycontact, :email,:usertype,:password,:password_confirmation)
+
       end
   end
-  
+ 
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,4 +16,6 @@ user.phone = '0302-3432321'
 user.emergencycontact = '0324-3265435'
 user.usertype='admin'
 user.save!
+
+#User.create(name: "Muskan Zehra", email: "muskan@gmail.com", password: "123456", password_confirmation: "123456", phone: "0302-3432321", emergencycontact: "0334-3432321", user_type: "admin")
 puts "seeding done..."
